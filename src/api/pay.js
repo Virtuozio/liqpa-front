4f378e8bc45e43da66fe52d22eefc645eeafa0b0
Nazarchyk Bogdan <bodjech2676@gmail.com>
2025-05-26 20:16:13 +0300

    quick check

--- src/api/pay.js
+++ src/api/pay.js
@@ -63,3 +63,6 @@ export const initInvoicePage = async (orderId) => {
 
 setInterval(startPayService, 300000);
 startPayService();
+if (window.location.pathname === "/complete.html") {
+  startPayService(); // одразу при завантаженні
+}
